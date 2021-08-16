eb36bbc8b718a2c1b943c1ad45f56d70cf081d4f
Julian Zambroni <julian.zambroni@gmail.com>
2021-08-16 20:51:18 -0300

    Cambio url otra vez

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
 const app = require("express")();
 const express = require("express");
-const http = require("http").createServer(app);
 const { normalize } = require("path");
 const helmet = require("helmet");
-const io = require("socket.io")(http, {
-  cors: {
-    origin: "https://chat-room-app-weld.vercel.app/",
-    methods: ["GET", "POST"],
-  },
-});
 const PORT = process.env.PORT || 8080;
 
 app.use(express.static(normalize(__dirname + "/public")));
 app.use(helmet());
 
 app.get("/*", (req, res) => {
-  res.sendFile(normalize(__dirname + "/public/index.html"));
+    res.sendFile(normalize(__dirname + "/public/index.html"));
 });
 
 var server = app.listen(PORT, () => {
-  console.log(`Listening on: ${PORT}`);
+    console.log(`Listening on: ${PORT}`);
+});
+
+const io = require("socket.io")(server, {
+    cors: {
+        origin: "https://chat-room-app-weld.vercel.app/",
+        methods: ["GET", "POST"],
+    },
 });
 
 io.on("connection", (socket) => {
-  socket.join("room1");
-  socket.on("mensaje", (mensaje) => console.log(mensaje));
+    socket.join("room1");
+    socket.on("mensaje", (mensaje) => console.log(mensaje));
 
-  socket.on("nuevoMensaje", (msg) => {
-    socket.to("room1").emit("nuevoMensaje", msg);
-  });
-  socket.on("disconnect", () => {
-    console.log("user disconnected", socket.id);
-  });
+    socket.on("nuevoMensaje", (msg) => {
+        socket.to("room1").emit("nuevoMensaje", msg);
+    });
+    socket.on("disconnect", () => {
+        console.log("user disconnected", socket.id);
+    });
 });
